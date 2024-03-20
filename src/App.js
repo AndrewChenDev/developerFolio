ae77c7c6ce3fe8a1e98db0fcaf83fdc910e5aad0
andrew840513 <andrew@ionblaster.com>
2024-03-20 04:26:44 -0700

    add tracking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import Hotjar from '@hotjar/browser';
 
+const siteId = 3913468;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 function App() {
   return (
     <div>
